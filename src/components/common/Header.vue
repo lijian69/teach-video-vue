0b4659816582539453b3f4719414692f35645ac2
Lijian <1326369406@qq.com>
2020-01-20 21:10:15 +0800

    更新

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <el-row :gutter="20">
-    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-    <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+  <el-row >
+    <el-col :span="2"><div class="grid-content"><img class="logo" src="../../assets/logo.png"></div></el-col>
+    <el-col :span="18"><div class="grid-content bg-purple"></div></el-col>
     <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
   </el-row>
 </template>
@@ -19,24 +19,16 @@
      margin-bottom: 0;
    }
   }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
   .bg-purple {
     background: #d3dce6;
   }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
+
   .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
+    min-height: 40px;
   }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
+  .logo {
+    height: 40px;
+    width: 100px;
   }
+
 </style>
